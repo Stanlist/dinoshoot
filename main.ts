7c76a7c4d86d1181bba741aa9b33093d1b24d7b8
Stanlist <39604199+Stanlist@users.noreply.github.com>
2020-09-18 14:54:17 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+scene.setBackgroundColor(5)
+let dino = sprites.create(img`
+    . . . . . . . . . . . . . 
+    . . . f f f f f f . . . . 
+    . f f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f c f f f c f f f . 
+    f c f f c c f f f c c f f 
+    f c c f f f f e f f f f f 
+    f f f f f f f e e f f f . 
+    f f e e f b f e e f f f . 
+    f f e 4 e 1 f 4 4 f f . . 
+    . f f f e 4 4 4 4 f . . . 
+    . 4 4 4 e e e e f f . . . 
+    . e 4 4 e 7 7 7 7 f . . . 
+    . f e e f 6 6 6 6 f f . . 
+    . f f f f f f f f f f . . 
+    . . f f . . . f f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(dino)
+let PixelsToMeters = 30
+let gravity = 9.8 * PixelsToMeters
+tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000101010101010101010102020202020202020202`, img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 
+    `, [myTiles.transparency16,sprites.castle.tilePath2,sprites.castle.tilePath5], TileScale.Sixteen))
+let PlayerStartPlace = tiles.getTilesByType(myTiles.transparency16)
